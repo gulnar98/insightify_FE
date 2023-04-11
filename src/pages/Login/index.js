a0c36c3fb804becd9b077e138b47f174e79c9d63
Elshad <elshadaghazade@gmail.com>
2023-04-11 17:15:08 +0400

    pages folderleri yaradildi

---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import style from './assets/css/style.module.css';
+
+export default function Login () {
+    return (
+        <>
+            <Head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Login page</title>
+            </Head>
+            
+            <h1>Login page</h1>
+        </>
+    );
+}
