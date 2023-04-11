3f4fa5e2f918f186d3c08fe4663b1d289d2a340f
Elshad <elshadaghazade@gmail.com>
2023-04-11 17:10:07 +0400

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { Profile } from "./Profile";
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: "yourAlchemyApiKey" }), publicProvider()]
+);
+
+// Set up client
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: "...",
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+});
+
+// Pass client to React Context Provider
+export default function Login() {
+  return (
+    <WagmiConfig client={client}>
+      <Profile />
+    </WagmiConfig>
+  );
+}
