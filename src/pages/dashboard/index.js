4ad19eaf456bf4fbcbf118a6960e19e5cb17d5c9
Ali Sadygov <alisadiqli001@gmail.com>
2023-04-18 18:27:22 +0400

    feat:connect rainbow

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -10,7 +10,6 @@ function Dashboard() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Dashboard page</title>
       </Head>
-
       <h1>Dashboard page</h1>
     </>
   );
