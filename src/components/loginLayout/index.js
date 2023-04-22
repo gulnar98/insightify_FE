25cc635eb79bb91588efce20db2ca015c9b1bfe6
nastakalow <nastakalow@gmail.com>
2023-04-22 18:11:23 +0400

    src/[200~Some css changes has been made in the login layout section.~

--- src/components/loginLayout/index.js
+++ src/components/loginLayout/index.js
@@ -1,9 +1,6 @@
 import LoginHeader from "../loginHeader";
 import style from "./style.module.css";
 
-import topLeft from "@//assets/images/login/top-right.svg";
-import bottomLeft from "@//assets/images/login/bottom-left.svg";
-
 function LoginLayout({ children }) {
   return (
     <>
