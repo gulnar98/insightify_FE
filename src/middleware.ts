8c968fcbd397edda080a56d8fb912187375c014b
Elshad <elshadaghazade@gmail.com>
2023-05-03 18:58:27 +0400

    api endpoint was added for retrieving records json

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
+import { NextApiRequest } from "next";
 
 async function loginMiddleware(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken");
@@ -22,9 +23,33 @@ async function loginMiddleware(request: NextRequest) {
   }
 }
 
+async function recordMiddleware(request: NextRequest) {
+
+  try {
+    const accessToken = request.cookies.get("accessToken");
+    const { payload } = await jwtVerify(accessToken.value, new TextEncoder().encode(process.env.JWT_SECRET));
+    if (!payload) {
+      throw 'Token is invalid';
+    } else {
+      return NextResponse.next();
+    }
+  } catch (error) {
+    return new Response(JSON.stringify({}), {
+      status: 401,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+}
+
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/login")) {
     return await loginMiddleware(request);
+  } else if (request.nextUrl.pathname.startsWith("/api/record/play")) {
+    return await recordMiddleware(request);
+  } else if (request.nextUrl.pathname.startsWith("/api/record/heatmap")) {
+    return await recordMiddleware(request);
   }
 
   const accessToken = request.cookies.get("accessToken");
@@ -49,5 +74,9 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|record).*)"]
+  matcher: [
+    "/api/record/play",
+    "/api/record/heatmap",
+    "/((?!api|_next/static|_next/image|favicon.ico|record).*)"
+  ]
 };
