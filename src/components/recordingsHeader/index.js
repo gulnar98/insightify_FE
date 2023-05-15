9eb5505d88e25263ac7d37428960a9a00881c17a
Ali Sadygov <alisadiqli001@gmail.com>
2023-05-15 23:02:17 +0400

    recordings componentleri hazirdir

---
+++ src/components/recordingsHeader/index.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import style from './assets/css/style.module.css'
+import Checkbox from '@mui/joy/Checkbox';
+
+export default function RecordingsHeader() {
+    return (
+        <>
+            <div className={style.container}>
+                <div className={style.children}>
+                    <div className={style.inputClick}>
+                        <Checkbox/>
+                    </div>
+                    <div className={style.avatar}>Avatar</div>
+                    <div className={style.userID}>User ID</div>
+                    <div className={style.label}>Label</div>
+                    <div className={style.topAssets}>Top 5 Assets</div>
+                    <div className={style.wallet}>Wallet</div>
+                    <div className={style.walletAge}>Wallet Age</div>
+                    <div className={style.totalAssets}>Total Assets</div>
+                    <div className={style.sourceOfFunds}>Source of funds</div>
+                    <div className={style.date}>Date</div>
+                    <div className={style.country}>Country</div>
+                    <div className={style.action}>Action #</div>
+                    <div className={style.pages}>Pages #</div>
+                    <div className={style.duration}>Duration</div>
+                    <div className={style.browser}>Browser</div>
+                    <div className={style.os}>OS</div>
+                    <div className={style.landingPage}>Landing Page</div>
+                    <div className={style.exitPage}>Exit Page</div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/recordingsInformation/index.js
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import style from './assets/css/style.module.css'
+import Checkbox from '@mui/joy/Checkbox';
+import playBtn from './assets/images/playBtn.png';
+import avatar from './assets/images/avatar.png'
+import wallet from './assets/images/wallet.png'
+import browser from './assets/images/browser.png'
+import os from './assets/images/os.png'
+import estonia from "./assets/images/estonia.png"
+import coinbase from './assets/images/coinbase.png'
+import other from './assets/images/other.png'
+
+export default function RecordingsInformation(
+    { labelBtn, 
+    topAssetsMirror, 
+    topAssetsEns, 
+    topAssetsApe, 
+    walletImage, 
+    walletAge, 
+    totalAssets,
+    dateProps,
+    countryProps,
+    actionProps,
+    pagesProps,
+    durationProps,
+    landingPage,
+    exitPage
+    }) {
+
+    return (
+        <>
+            <div className={style.container}>
+                <div className={style.children}>
+                    <div className={style.inputClick}>
+                        <Checkbox/>
+                        <button className={style.playBtn}>
+                            <img src={playBtn.src}/>&nbsp;
+                            Play
+                        </button>
+                    </div>
+                    <div className={style.avatar}>
+                        <img src={avatar.src}/>
+                    </div>
+                    <div className={style.userID}>
+                        <p className={style.nameID}>tural.eth</p>
+                        <p className={style.codeID}>0x312d..54e3</p>
+                    </div>
+                    <div className={style.label}>
+                        {labelBtn}
+                    </div>
+                    <div className={style.topAssets}>
+                        {topAssetsMirror}
+                        {topAssetsEns}
+                        {topAssetsApe}
+                    </div>
+                    <div className={style.wallet}>
+                        <img src={wallet.src}/>
+                    </div>
+                    <div className={style.walletAge}>
+                        {walletAge}
+                    </div>
+                    <div className={style.totalAssets}>
+                        {totalAssets}
+                    </div>
+                    <div className={style.sourceOfFunds}>
+                        <div>
+                            <div className={style.foundsOne}>
+                                <img src={coinbase.src}/>
+                                <p>Coinbase</p>
+                                <p>60%</p>
+                            </div>
+                            <div className={style.foundsTwo}>
+                                <img src={other.src}/>
+                                <p>Other</p>
+                                <p>40%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.date}>
+                        {dateProps}
+                    </div>
+                    <div className={style.country}>
+                        <img src={estonia.src}/>&nbsp;
+                        {countryProps}
+                    </div>
+                    <div className={style.action}>
+                        {actionProps}
+                    </div>
+                    <div className={style.pages}>
+                        {pagesProps}
+                    </div>
+                    <div className={style.duration}>
+                        {durationProps}
+                    </div>
+                    <div className={style.browser}>
+                        <img src={browser.src}/>
+                    </div>
+                    <div className={style.os}>
+                        <img src={os.src}/>
+                    </div>
+                    <div className={style.landingPage}>
+                        {landingPage}
+                    </div>
+                    <div className={style.exitPage}>
+                        {exitPage}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
