46a73ab6e07e04a527086a8acd9875c2136b5138
Ali Sadygov <alisadiqli001@gmail.com>
2023-05-17 15:21:29 +0400

    list-view-delete hazirdir

---
+++ src/components/avatar/index.js
@@ -0,0 +1,20 @@
+import style from "./assets/css/style.module.css"
+
+export default function Avatar({imgProps, nameProps, surnameProps}) {
+
+    let name = nameProps[0]
+    let surname = surnameProps[0]
+    console.log(Boolean(imgProps))
+
+    return (
+        imgProps ? 
+                    <>
+                        <img src={imgProps} className={style.avatarImg}/>
+                    </> :
+                    <>
+                        <div className={style.avatarNS}>
+                            {name}{surname}
+                        </div>
+                    </>
+    )
+}
